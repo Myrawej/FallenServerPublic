3dc28f2473fdfff0a49a701068bc65e4905346f5
Myrawej <xxxxxxunoxxxxxx@gmail.com>
2024-09-23 14:19:52 +0000

    Обновление фейковой структуры

---
+++ internal/userhandler/user_handler.go
@@ -0,0 +1,13 @@
+package main
+
+/*
+Информация о файле:
+Имя файла: user_handler.go
+Путь: internal/userhandler/user_handler.go
+Строк кода: 1
+Последнее изменение: Mon Sep 23 14:19:47 2024
+Размер: 0.00 КБ
+Язык: Go
+*/
+
+// Здесь было содержимое файла user_handler.go
