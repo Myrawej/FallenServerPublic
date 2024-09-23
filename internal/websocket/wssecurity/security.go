b04f4d259df9c964f5bd174e74991ee1a04ab23d
Myrawej <xxxxxxunoxxxxxx@gmail.com>
2024-09-23 17:15:49 +0000

    Обновление фейковой структуры

--- internal/websocket/wssecurity/security.go
+++ internal/websocket/wssecurity/security.go
@@ -5,8 +5,8 @@ package main
 Имя файла: security.go
 Путь: internal/websocket/wssecurity/security.go
 Строк кода: 349
-Последнее изменение: Mon Sep 23 17:15:38 2024
-Размер: 11.18 КБ
+Последнее изменение: Mon Sep 23 17:15:46 2024
+Размер: 11.19 КБ
 Язык: Go
 */
 
