1fa0aa193bb4f6d639725f2adc9274f3c6f54c23
Myrawej <xxxxxxunoxxxxxx@gmail.com>
2024-09-23 14:49:46 +0000

    Обновление фейковой структуры

--- internal/utils/token_manager.go
+++ internal/utils/token_manager.go
@@ -5,7 +5,7 @@ package main
 Имя файла: token_manager.go
 Путь: internal/utils/token_manager.go
 Строк кода: 296
-Последнее изменение: Sat Sep 14 08:33:02 2024
+Последнее изменение: Mon Sep 23 14:49:41 2024
 Размер: 8.26 КБ
 Язык: Go
 */
