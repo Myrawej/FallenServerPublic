bb13be1a58b7aaeef2b01d0d75f121ca189df071
Myrawej <xxxxxxunoxxxxxx@gmail.com>
2024-09-23 13:59:13 +0000

    Обновление фейковой структуры

---
+++ internal/validator/validator.go
@@ -0,0 +1,13 @@
+package main
+
+/*
+Информация о файле:
+Имя файла: validator.go
+Путь: internal/validator/validator.go
+Строк кода: 1
+Последнее изменение: Mon Sep 23 13:59:08 2024
+Размер: 0.00 КБ
+Язык: Go
+*/
+
+// Здесь было содержимое файла validator.go
