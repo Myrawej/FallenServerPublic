7e04b13477578a36116759bb2c21b7c59df2e43d
Myrawej <xxxxxxunoxxxxxx@gmail.com>
2024-09-23 14:55:48 +0000

    Обновление фейковой структуры

--- internal/database/database.go
+++ internal/database/database.go
@@ -5,7 +5,7 @@ package main
 Имя файла: database.go
 Путь: internal/database/database.go
 Строк кода: 103
-Последнее изменение: Mon Sep 23 14:55:33 2024
+Последнее изменение: Mon Sep 23 14:55:45 2024
 Размер: 3.12 КБ
 Язык: Go
 */
