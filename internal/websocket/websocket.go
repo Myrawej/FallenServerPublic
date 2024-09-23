f370f2a1e3d63aacd2bc83e56c3686f706373b53
Myrawej <xxxxxxunoxxxxxx@gmail.com>
2024-09-23 04:26:50 +0000

    Обновление фейковой структуры

--- internal/websocket/websocket.go
+++ internal/websocket/websocket.go
@@ -5,7 +5,7 @@ package main
 Имя файла: websocket.go
 Путь: internal/websocket/websocket.go
 Строк кода: 752
-Последнее изменение: Mon Sep 23 04:26:30 2024
+Последнее изменение: Mon Sep 23 04:26:45 2024
 Размер: 22.99 КБ
 Язык: Go
 */
