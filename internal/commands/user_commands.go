583c4916d3f2abadfccd46d84407018c3257c491
Myrawej <xxxxxxunoxxxxxx@gmail.com>
2024-09-23 14:38:32 +0000

    Обновление фейковой структуры

---
+++ internal/commands/user_commands.go
@@ -0,0 +1,13 @@
+package main
+
+/*
+Информация о файле:
+Имя файла: user_commands.go
+Путь: internal/commands/user_commands.go
+Строк кода: 1
+Последнее изменение: Mon Sep 23 14:38:26 2024
+Размер: 0.00 КБ
+Язык: Go
+*/
+
+// Здесь было содержимое файла user_commands.go
