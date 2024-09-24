a5bc371603476f3a0f66843ec25ea88550bf0607
Myrawej <xxxxxxunoxxxxxx@gmail.com>
2024-09-24 15:11:13 +0000

    Обновление фейковой структуры

---
+++ internal/database/context_keys.go
@@ -0,0 +1,13 @@
+package main
+
+/*
+Информация о файле:
+Имя файла: context_keys.go
+Путь: internal/database/context_keys.go
+Строк кода: 1
+Последнее изменение: Tue Sep 24 15:11:12 2024
+Размер: 0.00 КБ
+Язык: Go
+*/
+
+// Здесь было содержимое файла context_keys.go
