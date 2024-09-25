e8a8188f00a03c7211ff9d59344d1690caf43f88
Myrawej <xxxxxxunoxxxxxx@gmail.com>
2024-09-25 20:18:11 +0000

    Обновление фейковой структуры

--- internal/commands/command.go
+++ internal/commands/command.go
@@ -5,7 +5,7 @@ package main
 Имя файла: command.go
 Путь: internal/commands/command.go
 Строк кода: 143
-Последнее изменение: Wed Sep 25 20:04:59 2024
+Последнее изменение: Wed Sep 25 20:18:08 2024
 Размер: 4.18 КБ
 Язык: Go
 */
