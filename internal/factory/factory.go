93ccf02ab03a9cb09ff151e37c1b261e0e2b4951
Myrawej <xxxxxxunoxxxxxx@gmail.com>
2024-09-25 15:08:48 +0000

    Обновление фейковой структуры

--- internal/factory/factory.go
+++ internal/factory/factory.go
@@ -5,7 +5,7 @@ package main
 Имя файла: factory.go
 Путь: internal/factory/factory.go
 Строк кода: 90
-Последнее изменение: Wed Sep 25 15:08:11 2024
+Последнее изменение: Wed Sep 25 15:08:43 2024
 Размер: 2.55 КБ
 Язык: Go
 */
